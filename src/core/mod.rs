227902fb4605f9db818b0f80171923453bd2146d
Mark Wainwright <wainwrightml@gmail.com>
2022-05-27 09:36:24 +0100

    moved some files

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,22 +1,22 @@
-mod cube;
+mod cubie_cube;
 mod cube_invariant_getters;
 mod cube_invariant_setters;
 mod facelet_position;
 mod facelet_cube;
 mod misc;
 mod definitions;
-mod cornerposition;
-mod edgeposition;
+mod corner_position;
+mod edge_position;
 
 
 pub mod prelude {
-    pub use crate::core::cube::*;
+    pub use crate::core::cubie_cube::*;
     pub use crate::core::cube_invariant_getters::*;
     pub use crate::core::cube_invariant_setters::*;
     pub use crate::core::facelet_position::*;
     pub use crate::core::facelet_cube::*;
     pub use crate::core::misc::*;
     pub use crate::core::definitions::*;
-    pub use crate::core::cornerposition::*;
-    pub use crate::core::edgeposition::*;
+    pub use crate::core::corner_position::*;
+    pub use crate::core::edge_position::*;
 }
