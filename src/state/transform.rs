23e020159336844a07a0fd6fe1a07913804213fe
Mark Wainwright <wainwrightml@gmail.com>
2022-06-17 23:51:27 +0100

    Fixed typo

--- src/state/transform.rs
+++ src/state/transform.rs
@@ -86,7 +86,7 @@ impl Transform {
                 } else if y == &0.0 {
                     return format!("translateX({:.2}{})", x, unit);
                 }
-                format!(
+                return format!(
                     "translate({x:.2}{unit}, {y:.2}{unit})",
                     x = x,
                     y = y,
