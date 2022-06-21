743ddd83623f16d3ab6103a1f6243c1158a0c68d
Mark Wainwright <wainwrightml@gmail.com>
2022-06-21 22:19:43 +0100

    use criterion for benching

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,45 +1,50 @@
-#[macro_use]
-extern crate bencher;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::rc::Rc;
 
-use bencher::Bencher;
 use puzzle_cube::core::prelude::*;
 
-benchmark_group!(
+criterion_group!(
     benches,
     bench_create_move_table,
     bench_create_corner_slice_depth,
     bench_create_ud_edges_conjugation,
     bench_create_corner_symmetries,
     bench_create_flip_slice,
-    bench_create_phase_2_pruning
+    bench_create_phase_2_pruning,
+    //bench_solver
 );
-benchmark_main!(benches);
+criterion_main!(benches);
 
-fn bench_create_move_table(bench: &mut Bencher) {
-    bench.iter(create_move_table);
+fn bench_create_move_table(c: &mut Criterion) {
+    c.bench_function("create move table", |bench|bench.iter(create_move_table));    
 }
 
-fn bench_create_corner_symmetries(bench: &mut Bencher) {
-    bench.iter(create_corner_symmetries);
+fn bench_create_corner_symmetries(c: &mut Criterion) {
+    c.bench_function("create corner symmetries", |bench|bench.iter(create_corner_symmetries));      
 }
 
-fn bench_create_flip_slice(bench: &mut Bencher) {
-    bench.iter(create_flip_slice);
+fn bench_create_flip_slice(c: &mut Criterion) {
+    c.bench_function("create flip slice", |bench|bench.iter(create_flip_slice));  
 }
 
-fn bench_create_ud_edges_conjugation(bench: &mut Bencher) {
-    bench.iter(create_ud_edges_conjugation);
+fn bench_create_ud_edges_conjugation(c: &mut Criterion) {
+    c.bench_function("create ud edges conjugation", |bench|bench.iter(create_ud_edges_conjugation));    
 }
 
-fn bench_create_corner_slice_depth(bench: &mut Bencher) {
+fn bench_create_corner_slice_depth(c: &mut Criterion) {
     let moves_source = MovesSource::create();
-    bench.iter(|| create_corner_slice_depth(&moves_source));
+    c.bench_function("create corner slice depth", |bench|bench.iter(||create_corner_slice_depth(&moves_source)));
 }
 
-fn bench_create_phase_2_pruning(bench: &mut Bencher) {
+fn bench_create_phase_2_pruning(c: &mut Criterion) {
     let moves_source = MovesSource::create();
     let corner_source = CornerSymmetriesSource::create();
-    bench.iter(|| create_phase_2_pruning(&moves_source, &corner_source));
+    c.bench_function("create phase 2 pruning", |bench|bench.iter(||create_phase_2_pruning(&moves_source, &corner_source)));    
+}
+
+fn bench_solver(c: &mut Criterion) {
+    let data_source = Rc::new(DataSource::create());
+    c.bench_function("solve random cube", |bench|bench.iter(||solve(data_source.clone())));
 }
 
 fn create_move_table() -> MovesSource {
@@ -67,4 +72,12 @@ fn create_phase_2_pruning(
     corner_source: &CornerSymmetriesSource,
 ) -> Vec<u32> {
     DataSource::create_phase_2_pruning(move_source, corner_source)
+}
+
+fn solve(data_source: Rc<DataSource>) {
+    for seed in 0..1 {
+        let base_cube = CubieCube::random_cube(seed);
+        let solution = Solver::get_solution(base_cube, data_source.clone(), SolveSettings::default());
+        assert!(solution.is_some());
+    }
 }
